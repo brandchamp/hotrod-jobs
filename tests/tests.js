33fb4b041811c65f7d4ce7eddcec3de25b33c6eb
Andrew Smith <emertechie@gmail.com>
2015-08-05 13:14:43 +0700

    Enable signalling long running jobs to stop. Add tests for runOnce and runContinuous

--- tests/tests.js
+++ tests/tests.js
@@ -5,6 +5,10 @@ var jobs = require('../lib/index');
 
 describe('Creating & Running Jobs', function() {
 
+    afterEach(function() {
+        assert.equal(jobs._getCurrentlyRunningCount(), 0, 'No jobs left running');
+    });
+
     it('gives jobs a name to help with logging', function() {
         var job = jobs.create('MyJob', function(resolve, reject, logger) {
             resolve('good job');
@@ -21,7 +25,7 @@ describe('Creating & Running Jobs', function() {
             jobs.runOnce(job).then(function(result) {
                 tryAssert(function() {
                     assert.equal(result, 'good job');
-                }, done);
+                }, done, done);
             });
         });
 
@@ -34,7 +38,7 @@ describe('Creating & Running Jobs', function() {
             }, function(err) {
                 tryAssert(function() {
                     assert.equal(err, 'blerf');
-                }, done);
+                }, done, done);
             });
         });
 
@@ -48,7 +52,32 @@ describe('Creating & Running Jobs', function() {
             }, function(err) {
                 tryAssert(function() {
                     assert.equal(err.message, 'blerf');
-                }, done);
+                }, done, done);
+            });
+        });
+
+        it('can signal long running job to stop', function(done) {
+            var job = jobs.create('MyJob', function(resolve, reject) {
+                this.on('stop', function(stopped) {
+                    stopped('stop return value');
+                    // Note: can optionally call resolve / reject here if needed depending on use case
+                });
+            });
+
+            var jobStopped = false;
+
+            jobs.runOnce(job).then(function() {
+                done('should not be called');
+            }, function(err) {
+                tryAssert(function() {
+                    assert.isTrue(jobStopped, 'jobStopped');
+                    assert.equal(err.message, 'force-stopped');
+                }, done, done);
+            });
+
+            job.stop(function stopped(arg) {
+                assert.equal(arg, 'stop return value');
+                jobStopped = true;
             });
         });
     });
@@ -70,7 +99,7 @@ describe('Creating & Running Jobs', function() {
                     setTimeout(function() {
                         tryAssert(function() {
                             assert.equal(runCount, MAX_RUNS, 'No more job runs');
-                        }, done);
+                        }, done, done);
                     }, 300);
                 }
             };
@@ -86,6 +115,52 @@ describe('Creating & Running Jobs', function() {
 
             var intervalInSecs = 0.01;
             stopFn = jobs.runContinuous(job, intervalInSecs);
+        });
+
+        it('can force stop a continuous job which will signal long running job to stop', function(done) {
+            var stopFn;
+            var runCount = 0;
+            // Note: need to wait for second run so that stopFn variable is set
+            var EXPECTED_RUN_COUNT = 2;
+
+            var onJobStarted = function() {
+                if (++runCount !== EXPECTED_RUN_COUNT) {
+                    return;
+                }
+
+                var force = true;
+                var stopPromise = stopFn(force);
+
+                stopPromise.then(function(result) {
+                    tryAssert(function() {
+                        assert.equal(result, 'stop return value');
+                        assert.equal(runCount, EXPECTED_RUN_COUNT, 'Run one time');
+
+                        // Wait a little bit to make sure it really has stopped
+                        setTimeout(function() {
+                            tryAssert(function() {
+                                assert.equal(runCount, EXPECTED_RUN_COUNT, 'No more job runs');
+                            }, done, done);
+                        }, 300);
+                    }, done);
+                }, function(err) {
+                    done('should not be called');
+                });
+            };
+
+            var longRunningJob = jobs.create('MyJob', function(resolve, reject) {
+                this.on('stop', function(stopped) {
+                    stopped('stop return value');
+                });
+                onJobStarted();
+                // Need to finish one run ok, then make it long running
+                if (runCount === 1) {
+                    resolve();
+                }
+            });
+
+            var intervalInSecs = 0.01;
+            stopFn = jobs.runContinuous(longRunningJob, intervalInSecs);
         });
 
         it('will continue to run job multiple times even if it occasionally fails', function(done) {
@@ -111,11 +186,13 @@ describe('Creating & Running Jobs', function() {
     });
 });
 
-function tryAssert(assertFn, done) {
+function tryAssert(assertFn, onError, onSuccess) {
     try {
         assertFn();
-        done();
+        if (onSuccess) {
+            onSuccess();
+        }
     } catch (e) {
-        done(e);
+        onError(e);
     }
 }
